e8bcea28a8168aae6272999a574269ecb91b0d46
Joppe Aarts <joppe@zicht.nl>
2015-10-07 13:40:06 +0200

    dev

---
+++ web/js/system.conf.js
@@ -0,0 +1,20 @@
+/*global System*/
+
+System.config({
+    baseURL: '../',
+
+    transpiler: 'babel',
+
+    map: {
+        jquery: 'vendor/jquery/dist/jquery',
+        underscore: 'vendor/underscore/underscore',
+        backbone: 'vendor/backbone/backbone',
+        react: 'vendor/react/react'
+    },
+
+    meta: {
+        backbone: {
+            deps: ['jquery', 'underscore']
+        }
+    }
+});
