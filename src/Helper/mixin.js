a801ee9b6fdd0a2da60304ae85caae7fc18a65e5
Joppe Aarts <joppe@zicht.nl>
2015-10-09 17:56:42 +0200

    dev

--- src/Helper/mixin.js
+++ src/Helper/mixin.js
@@ -4,14 +4,17 @@ import _ from 'underscore';
 let mixin = {
     componentDidMount() {
         // listen to changes
+        // is it a model or a collection?
     },
 
     componentWillUnmount() {
         // stop listening to changes
+        this.stopListening();
     },
 
     componentWillReceiveProps(props) {
         // stop listening
+        this.stopListening();
         // start listening
     }
 };
